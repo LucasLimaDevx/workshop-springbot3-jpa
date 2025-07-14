5d69480aa44b92ac07ed632030809c15bc79652e
Lucas Lima <lucaslimadevx@gmail.com>
2025-07-14 10:35:30 -0300

    Project created

---
+++ src/test/java/com/educamais/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educamais.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
