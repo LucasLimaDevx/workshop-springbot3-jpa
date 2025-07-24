d18a045cf05d58d09067a2f6ab1b29434362b8af
Lucas Lima <lucaslimadevx@gmail.com>
2025-07-24 17:24:06 -0300

    Exception handling - update

--- src/main/java/com/educamais/course/services/UserService.java
+++ src/main/java/com/educamais/course/services/UserService.java
@@ -12,6 +12,8 @@ import com.educamais.course.repositories.UserRepository;
 import com.educamais.course.services.exceptions.DatabaseException;
 import com.educamais.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -43,10 +45,15 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			
+			return repository.save(entity);
+		}
+		catch(EntityNotFoundException e ) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData( User entity, User obj) {
